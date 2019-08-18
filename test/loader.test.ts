125d410ceb4bd34ffb8beea57481310a92841a6c
aquariuslt <superaquariuslt@gmail.com>
2019-08-18 21:10:00 +0800

    test: update jest configuration

--- test/loader.test.ts
+++ test/loader.test.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import * as loader from '../src/index';
+import * as loader from '@/index';
 
 const sampleSource = fs.readFileSync(path.join(__dirname, './__fixtures__/sample.properties')).toString();
 
